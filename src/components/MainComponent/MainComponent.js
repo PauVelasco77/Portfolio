ddb6fd591891f9db515d6d0f73383ebc55fbbe9f
PauVelasco77 <pau.garrofe@cognativinc.com>
2022-07-13 12:12:16 +0200

    added mobileResponsive on NavBar

--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -1,9 +1,14 @@
+import styled from "styled-components";
 import NavBar from "../NavBar/NavBar";
+
+const MainComponentContainer = styled.section``;
 
 const MainComponent = () => {
   return (
     <>
-      <NavBar />
+      <MainComponentContainer>
+        <NavBar />
+      </MainComponentContainer>
     </>
   );
 };
