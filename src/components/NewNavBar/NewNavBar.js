eef9c95db8f6f34c5440c863464a3c7ee676d800
PauVelasco77 <pau.garrofe@cognativinc.com>
2022-07-14 10:29:58 +0200

    added profile name in nav bar

--- src/components/NewNavBar/NewNavBar.js
+++ src/components/NewNavBar/NewNavBar.js
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material";
+import { Avatar, Box, Paper, Typography } from "@mui/material";
 import styled from "@emotion/styled";
 
 const NavContainer = styled(Paper)`
@@ -7,10 +7,43 @@ const NavContainer = styled(Paper)`
   width: 250px;
   position: fixed;
   border-radius: 0 15px 15px 0;
+  font-family: "Roboto", sans-serif;
+  font-size: 40px;
+`;
+
+const Profile = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 80%;
+  margin: 15px auto;
+  padding: 10px;
+  border-radius: 7.5px;
+  transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    background-color 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition-property: box-shadow, background-color;
+  transition-duration: 200ms, 200ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1),
+    cubic-bezier(0.4, 0, 0.2, 1);
+  transition-delay: 0ms, 0ms;
+
+  :hover {
+    background-color: rgba(255, 255, 255, 0.2);
+    cursor: pointer;
+  }
 `;
 
 const NewNavBar = () => {
-  return <NavContainer elevation={0} sx={{ maxWidth: 256 }}></NavContainer>;
+  return (
+    <NavContainer elevation={0} sx={{ maxWidth: 256 }}>
+      <Profile>
+        <Avatar src="../../../profilePhoto2.jpg" alt="avatar photo" />
+        <Typography sx={{ color: "#fff", fontWeight: 300, fontSize: 14 }}>
+          Pau Velasco Garrofé
+        </Typography>
+      </Profile>
+    </NavContainer>
+  );
 };
 
 export default NewNavBar;
